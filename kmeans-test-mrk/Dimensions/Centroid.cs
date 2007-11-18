4182f0f49c9329b117dc5791ef9d8277251c4664
arcusangelus <arcusangelus@aae48506-7a3d-0410-b332-91b007856486>
2007-11-18 11:06:08 +0000

    initial commit of Mike's kmeans framework test code

---
+++ kmeans-test-mrk/Dimensions/Centroid.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClusteringFramework.Dimensions
+{
+    public class Centroid : Point
+    {
+        public Centroid()
+            : base(null, null)
+        {
+        }
+    }
+}
---
+++ kmeans-test-mrk/Dimensions/PlaneType.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClusteringFramework.Dimensions
+{
+    public abstract class PlaneType
+    {
+    }
+}
